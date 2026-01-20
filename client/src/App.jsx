e0ecd4f0456bbdb1232f55615e62200708f80f39
Alok <mishraalok189381@gmail.com>
2026-01-20 19:20:06 +0530

    setup tailwind and vite react and motion

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='min-h-screen bg-black text-white flex justify-center'>
+      Avail
+    </div>
+  )
+}
+
+export default App
