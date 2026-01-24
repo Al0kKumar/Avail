3754411425f0a886d61557420af45606b0c4851d
Alok <mishraalok189381@gmail.com>
2026-01-24 16:26:46 +0530

    updated env

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,10 +2,12 @@ import app from './app.js';
 import { connectDB } from './config/db.js';
 import { env } from './config/env.js';
 
+const PORT = env.PORT || 3000;
+
 const startServer = async () => {
   await connectDB();
-  app.listen(env.PORT, () =>
-    console.log(`Server running on port ${env.PORT}`)
+  app.listen(PORT, () =>
+    console.log(`Server running on port ${PORT}`)
   );
 };
 
