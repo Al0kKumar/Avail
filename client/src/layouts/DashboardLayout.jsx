5f50c3330dc24e77b007fbad99fa285523fcce6d
Alok <mishraalok189381@gmail.com>
2026-01-24 16:45:17 +0530

    updated import

--- client/src/layouts/DashboardLayout.jsx
+++ client/src/layouts/DashboardLayout.jsx
@@ -1,5 +1,5 @@
-import Sidebar from '../components/Sidebar';
-import Topbar from '../components/Topbar';
+import Sidebar  from '../components/SideBar';
+import Topbar from '../components/TopBar';
 
 export default function DashboardLayout({ children }) {
   return (
